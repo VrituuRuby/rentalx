1f835516bcee92147db329a510875119a6b93541
VictorVelozo <victor.velozo@outlook.com>
2023-05-23 18:09:10 -0300

    fix: changes on refresh-token

--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -1,24 +1,28 @@
-import { verify } from "jsonwebtoken";
+import { sign, verify } from "jsonwebtoken";
 import { inject } from "tsyringe";
 
 import auth from "@config/auth";
 import { IUserTokensRepository } from "@modules/accounts/repositories/IUserTokensRepository";
 import { AppError } from "@shared/errors/AppError";
+import { IDateProvider } from "@shared/providers/DateProvider/IDateProvider";
 
 interface IPayload {
-  sub: string;
   email: string;
+  sub: string;
 }
 
 export class RefreshTokenUseCase {
   constructor(
     @inject("UserTokensRepository")
     private userTokensRepository: IUserTokensRepository,
+    @inject("DateProvider")
+    private dateProvider: IDateProvider,
   ) {}
-  async execute(token: string) {
-    const { email, sub } = verify(token, auth.refresh_token_secret) as IPayload;
-
-    const user_id = sub;
+  async execute(token: string): Promise<string> {
+    const { email, sub: user_id } = verify(
+      token,
+      auth.refresh_token_secret,
+    ) as IPayload;
 
     const userToken = await this.userTokensRepository.findByUserIDAndToken(
       user_id,
@@ -28,5 +32,18 @@ export class RefreshTokenUseCase {
     if (!userToken) throw new AppError("User's refresh token doesn't exists");
 
     await this.userTokensRepository.deleteById(userToken.id);
+
+    const refresh_token = sign({ email }, auth.refresh_token_secret, {
+      subject: user_id,
+      expiresIn: auth.refresh_token_expires_in,
+    });
+
+    await this.userTokensRepository.create({
+      user_id,
+      expire_date: this.dateProvider.addDays(auth.expire_days),
+      refresh_token,
+    });
+
+    return refresh_token;
   }
 }
